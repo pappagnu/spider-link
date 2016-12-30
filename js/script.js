48fc75c00b8f42ea9a31727f90f789e6a237cbf3
nicfue <nicolasfuentes@hotmail.com>
2016-12-31 00:45:33 +0100

    lagt till parallax på mobilen och lagt till marginaler och paddings

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ function changeMenu() {
         }
     }
 
-    if (scrollBarPosition > 830) {
+    if (scrollBarPosition > 610) {
         navLinks[1].style.backgroundColor = 'rgba(223, 72, 89, 0.5)';
         for (let i = 0; i < navLinks.length; i++) {
             if (i != 1) {
@@ -55,7 +55,7 @@ function changeMenu() {
         }
     }
 
-    if (scrollBarPosition > 1600) {
+    if (scrollBarPosition > 1650) {
         navLinks[2].style.backgroundColor = 'rgba(223, 72, 89, 0.5)';
         for (let i = 0; i < navLinks.length; i++) {
             if (i !== 2) {
@@ -64,7 +64,7 @@ function changeMenu() {
         }
     }
 
-    if (scrollBarPosition > 2600) {
+    if (scrollBarPosition > 2750) {
         navLinks[3].style.backgroundColor = 'rgba(223, 72, 89, 0.5)';
         for (let i = 0; i < navLinks.length; i++) {
             if (i !== 3) {
@@ -92,4 +92,21 @@ function changeMenu() {
     } else {
         scroller.style.display = "none";
     }
+}
+
+// CONTACT MAP
+function initMap() {
+  let mapCanvas = document.getElementById("map");
+  let cord = new google.maps.LatLng(33.1791817, -81.0961495);
+  let mapOptions = {
+    center: cord,
+    zoom: 13,
+    scrollwheel: false
+  };
+  let map = new google.maps.Map(mapCanvas, mapOptions);
+  let marker = new google.maps.Marker({
+  position: cord,
+  title:"Vi finns på Spider Road"
+  });
+  marker.setMap(map);
 }
